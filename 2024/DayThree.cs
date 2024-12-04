7aa0ea9f3131e83abaefcfc62e1eb75b31fb5dba
Mitchell <mitchellscott@me.com>
2024-12-04 17:53:32 -0600

    day three

---
+++ 2024/DayThree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace advent_of_code
+{
+    public class DayThree
+    {
+        public static int ProblemOne(string inputString)
+        {
+            string pattern = @"mul\((\d+),(\d+)\)";
+            int total = 0;
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(inputString);
+
+            foreach (Match match in matches)
+            {
+                int num1 = int.Parse(match.Groups[1].Value);
+                int num2 = int.Parse(match.Groups[2].Value);
+                int result = num1 * num2;
+                total += result;
+            }
+            Console.WriteLine($"Total: {total}");
+            return total;
+        }
+        //public static int ProblemTwo(string input)
+        //{
+
+        //}
+    }
+}
--- 2024/Program.cs
+++ 2024/Program.cs
@@ -11,9 +11,11 @@ namespace _2024
             //Console.WriteLine(DayOne.ProblemOne(input1));
             //Console.WriteLine(DayOne.ProblemTwo(input1));
             //string[] input2 = File.ReadAllLines("./Resources/input2.txt");
-            string[] input2 = new string[] { "7 6 4 2 1", "1 2 7 8 9", "9 7 6 2 1", "1 3 2 4 5", "8 6 4 4 1", "1 3 6 7 9" };
+            //string[] input2 = new string[] { "7 6 4 2 1", "1 2 7 8 9", "9 7 6 2 1", "1 3 2 4 5", "8 6 4 4 1", "1 3 6 7 9" };
+            string input3 = File.ReadAllText("./Resources/input3.txt");
             //Console.WriteLine(DayTwo.ProblemOne(input2));
-            Console.WriteLine(DayTwo.ProblemTwo(input2));
+            //Console.WriteLine(DayTwo.ProblemTwo(input2));
+            Console.WriteLine(DayThree.ProblemOne(input3));
         }
     }
 }
